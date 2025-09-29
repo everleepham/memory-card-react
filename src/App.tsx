dc159daba690bd4be473e88eef046c9cbac014eb
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-09-29 10:53:36 +0200

    update App and Card Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 import CardComp from "./components/CardComp"
+import cards from "./data/cards.json"
+import type { TCard, TCardList } from "./types/card.types"
+import { useState } from "react"
+
 
 const App = () => {
+
+
+	const createGameCards = (): TCardList => {
+		const pairs = cards.flatMap((card) => [
+			{ ...card, id: card.id },
+			{ ...card, id: card.id + 100 },
+		])
+		return pairs
+	}
+
+	const suffleCards = (card: TCardList): TCardList => {
+		return card.sort(() => Math.random() - 0.5)
+	}
+
+	const [gameCards, setGameCards] = useState<TCardList>(createGameCards())
+	console.log(gameCards)
+
+	const handleCardClick = (clickedCard: TCard) => {
+		// Flip the card
+		setGameCards((prev) =>
+			prev.map((card) =>
+				card.id === clickedCard.id ? { ...card, flipped: !card.flipped} : card
+			)
+		)
+	}
+
 	return (
-		<div>
+		<div className="main_section">
 			<h1>Memory Game</h1>
-			<CardComp />
+			<div className="card_container">
+				{gameCards.map((card: TCard) => {
+					return <CardComp card={card} clickProp={() => {}} key={card.id} />
+				})}
+			</div>
 		</div>
 	)
 }
--- src/components/CardComp.tsx
+++ src/components/CardComp.tsx
@@ -1,13 +1,24 @@
 import styles from "./CardComp.module.css"
+import type { TCard } from "../types/card.types"
 
-const CardComp = () => {
+
+export type TCardProps = {
+	clickProp: (card: TCard) => void
+	card: TCard
+}
+
+const CardComp = ({ clickProp, card }: TCardProps) => {
 	const handleClick = () => {
-		console.log("clicked !!")
+		console.log("clicked", card)
+		clickProp(card) 
 	}
 
 	return (
-		<article onClick={handleClick} className={styles.card}>
-			CardComp
+		<article
+			onClick={handleClick}
+			className={`${styles.card} ${card.flipped ? styles.animate__rotate : ""}`}
+		>
+			<img src={`./imgs/${card.image}`} alt={card.name}/>
 		</article>
 	)
 }
